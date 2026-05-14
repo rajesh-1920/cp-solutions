f5d8d8b3717635cf7babeac5bff149533d6fc608
rajesh-1920 <rajesh18@cse.pstu.ac.bd>
2026-05-14 15:14:49 +0600

    CF camp Day-01 compelete

---
+++ 90days-cf-camp-sohojcoding/day-01/A_Bit.cpp
@@ -0,0 +1,49 @@
+// Author   : Rajesh Biswas
+// CF       : rajesh_1920
+// Date     : 14.05.2026
+// Problem  : https://codeforces.com/problemset/problem/282/A
+
+#include <bits/stdc++.h>
+using namespace std;
+//----------------------------(definition section)-----------------------------------------
+#define dbg(x) cout << #x << " = " << x << '\n'
+#define int long long int
+#define fi first
+#define sc second
+
+#define all(s) s.begin(), s.end()
+#define rall(s) s.rbegin(), s.rend()
+
+const double eps = 1e-1;
+const int inf = 9e16 + 7;
+const int MOD = 1e9 + 7;
+const int N = 1e5 + 10;
+//-----------------------------------------------------------------------------------------
+void solve(void)
+{
+    int n, ans = 0;
+    cin >> n;
+    while (n--)
+    {
+        string s;
+        cin >> s;
+        sort(all(s));
+        ans += s.front() == '+' ? 1 : -1;
+    }
+    cout << ans << '\n';
+}
+//-----------------------------------------------------------------------------------------
+signed main()
+{
+    // cout << fixed << showpoint << setprecision(10);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int test = 1, T;
+    // cin >> test;
+    for (T = 1; T <= test; T++)
+    {
+        // cout << "Case " << T << ": ";
+        solve();
+    }
+    return 0;
+}
