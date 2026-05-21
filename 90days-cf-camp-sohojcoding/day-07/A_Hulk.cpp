4bf476b727ebe4dcce9b11bbbbe80b4ab189ffe9
rajesh-1920 <rajesh18@cse.pstu.ac.bd>
2026-05-21 19:16:30 +0600

    CF camp Day-07 compelete

---
+++ 90days-cf-camp-sohojcoding/day-07/A_Hulk.cpp
@@ -0,0 +1,47 @@
+// Author   : Rajesh Biswas
+// CF       : rajesh_1920
+// Date     : 20.05.2026
+// Problem  : https://codeforces.com/problemset/problem/705/A
+
+#include <bits/stdc++.h>
+using namespace std;
+//----------------------------(definition section)-----------------------------------------
+#define dbg(x) cout << #x << " = " << x << '\n'
+#define int long long int
+#define fi first
+#define sc second
+
+#define all(s) s.begin(), s.end()
+#define rall(s) s.rbegin(), s.rend()
+
+const double eps = 1e-1;
+const int inf = 9e16 + 7;
+const int MOD = 1e9 + 7;
+const int N = 1e5 + 10;
+//-----------------------------------------------------------------------------------------
+void solve(void)
+{
+    int n;
+    string ans;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        ans += (i & 1) ? "I love that " : "I hate that ";
+    ans.pop_back(), ans.pop_back(), ans.pop_back(), ans.pop_back(), ans.pop_back();
+    ans += "it";
+    cout << ans << '\n';
+}
+//-----------------------------------------------------------------------------------------
+signed main()
+{
+    cout << fixed << showpoint << setprecision(12);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int test = 1, T;
+    // cin >> test;
+    for (T = 1; T <= test; T++)
+    {
+        // cout << "Case " << T << ": ";
+        solve();
+    }
+    return 0;
+}
